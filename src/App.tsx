36030602085ca95e059d777a520a765bdf094961
Alex Rodriguez <xalecr30@gmail.com>
2025-04-13 11:15:01 -0400

    agregado chakra ui y react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the React App</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          This is a simple React application.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </header>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { Provider } from './components/ui/provider.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 )
